df1c98768c766ed7f883b3802718b17802578011
Walter Branning <walter.d.branning@icloud.com>
2023-08-15 12:36:34 -0500

    Merging

---
+++ site-app (copy)/src/components/Header.js
@@ -0,0 +1,45 @@
+// Component to create the Header Bar with navigation links
+import {useState} from 'react';
+import 'primeicons/primeicons.css'
+
+function changeTheme (e) {
+    let root = document.documentElement;
+    let new_color = getComputedStyle(root).getPropertyValue(`--brand-${e}`);
+    let new_color_light = getComputedStyle(root).getPropertyValue(`--brand-${e}-light`);
+    let new_color_glow = getComputedStyle(root).getPropertyValue(`--brand-${e}-glow`);
+    root.style.setProperty('--brand-color', new_color);
+    root.style.setProperty('--brand-color-light', new_color_light);
+    root.style.setProperty('--brand-color-glow', new_color_glow);
+}
+
+function HeaderComponent () {
+
+    // const [brandColor, setBrandColor] = useState('Hello Brand Color!');
+    // console.log(brandColor)
+
+    return (
+        <header >
+            <div className='header-content'>
+                <div className='headerLinks' id='navLinks'>
+                    <a href='#' className='nav-link highlight'>What</a>
+                    <a href='#' className='nav-link highlight'>Professional</a>
+                    <a href='#' className='nav-link highlight'>Projects</a>
+                    <a href='#' className='nav-link highlight'>Who</a>
+                </div>
+                <div className='headerLinks' id='extraLinks'>
+                    <div className='theme-bar'>
+                        <button className='theme-button' id='green' onClick={() => changeTheme('green')}></button>
+                        <button className='theme-button' id='blue' onClick={() => changeTheme('blue')}></button>
+                        <button className='theme-button' id='red' onClick={() => changeTheme('red')}></button>
+                        <button className='theme-button' id='orange'onClick={() => changeTheme('orange')}></button>
+                    </div>
+                    <a href='https://www.linkedin.com/in/walter-branning' className='link-icon nav-link highlight pi pi-linkedin'></a>
+                    <a href='https://github.com/WaltBranning' className='link-icon nav-link highlight pi pi-github'></a>
+                    <a href='mailto:Walter@oddlygeek.com' className='link-icon nav-link highlight pi pi-envelope'></a>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default HeaderComponent;
