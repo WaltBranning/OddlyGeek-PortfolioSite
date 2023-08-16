5b902184a8d35f5a6bfee1f73e5c18ad88d87b39
Walter Branning <walter.d.branning@icloud.com>
2023-08-15 21:50:27 -0500

    Created a toggable intro message

--- site-app/src/components/Content.js
+++ site-app/src/components/Content.js
@@ -19,11 +19,11 @@ function ContentComponent () {
                     
 
                     </div>
-                        <div id='intro-content'>
+                        <div id='intro-content' className='toggle-state'>
                             <p><span className='highlight'>Who am I, you ask?</span> Well, I'm a devoted husband and father, my family holds the utmost importance in my life and I cherish this job most of all. Becoming the best provider and role model for them is a job I take very seriously. Striving for excellence not only aligns with my natural desire to take pride in my work but also ensures that my family, too, can take pride in what I do.
                             </p>
                             <p>
-                            I'm a naturally curious individual with a passion for solving problems and for creating things. I find immense joy in the process of creation and bringing ideas to life. Whether it's dabbling in 3D printing, tackling software challenges, or even venturing into the world of carpentry to build a cozy cabin, I'm always up for a new adventure. I'm fascinated by it all, and you'll often find me engrossed in projects that blend creativity with practicality. My diverse interests and experiences have shaped me into an adaptable and versatile professional, which are crucial qualities in the ever-evolving field of software engineering.</p>
+                            I'm a naturally curious individual with a passion for solving problems and for creating things. I find immense joy in the process of creation and bringing ideas to life. Whether it's dabbling in 3D printing, tackling software challenges, or venturing into the world of carpentry to build a cozy cabin, I'm always up for a new adventure. I'm fascinated by it all, and you'll often find me engrossed in projects that blend creativity with practicality. My interests and experiences have shaped me into an adaptable and versatile professional, valuable qualities in the ever-evolving field of software engineering.</p>
                         </div>
                 </div>
 
@@ -31,8 +31,9 @@ function ContentComponent () {
 
                     <div id='intro-more-content'>
                         <span className='highlight hover-glow' onClick={()=>{
-                            console.log("Clicked See More")
-                        }}>See More</span>
+                            var tog = document.getElementById('intro-content');
+                            tog.classList.toggle("toggle-state");
+                        }}>Read More</span>
                     </div>
 
                 </div>
